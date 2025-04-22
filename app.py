e9e3bcdf44b9d3498924dbbfd92d65f7e10090b7
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2025-04-22 10:56:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ import os
 short_value = os.environ.get('Short', 'not found')
 normal_value = os.environ.get('Normal', 'not found')
 long_value = os.environ.get('Long', 'not found')
+QUARTO_PROFILE = os.environ.get('QUARTO_PROFILE', 'not found')
+R_CONFIG_ACTIVE = os.environ.get('R_CONFIG_ACTIVE', 'not found')
 
 # Display text with labels
 st.text(f"Short: {short_value}")
 st.text(f"Normal: {normal_value}")
 st.text(f"Long: {long_value}")
+st.text(f"QUARTO_PROFILE: {QUARTO_PROFILE}")
+st.text(f"R_CONFIG_ACTIVE : {R_CONFIG_ACTIVE}")
